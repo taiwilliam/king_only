3d354066ae0d317fa829c08f9c065478f9d3ef54
William Tai <44943338+taiwilliam@users.noreply.github.com>
2023-03-02 16:02:34 +0800

    william考試結束

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let imgDom = document.querySelector('.js-img')
+let imgDomH = imgDom.offsetHeight
+let textWrapDom = document.querySelector('.js-text-wrap')
+
+textWrapDom.style.height = imgDomH + 'px';
